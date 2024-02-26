280cf60b2c27a5ef8d27093fe60d980116c28e30
jeffreyrene <jeffreyrene575@gmail.com>
2024-02-27 00:00:00 +0500

    chore: update friendly mento voice

--- lesson_plans/templatetags/filename_filters.py
+++ lesson_plans/templatetags/filename_filters.py
@@ -10,3 +10,4 @@ def basename(value):
 # touched on 2025-06-13T18:49:51.142824Z
 # touched on 2025-06-13T18:50:17.509971Z
 # touched on 2025-06-13T18:50:59.967500Z
+# touched on 2025-06-13T18:51:05.858691Z
